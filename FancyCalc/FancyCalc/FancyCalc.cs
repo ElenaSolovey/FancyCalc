f464f0d169bd7bb683f28c6a16cbfc0c49ad9102
HelenSolovey <47430048+HelenSolovey@users.noreply.github.com>
2019-02-18 22:57:54 +0300

    Add files via upload

---
+++ FancyCalc/FancyCalc/FancyCalc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FancyCalc
+{
+    public class FancyCalcEnguine
+    {
+
+        public double Add(double a, double b)
+        {
+            //throw new NotImplementedException();
+            return a + b;
+        }
+
+
+        public double Subtract(double a, double b)
+        {
+            //throw new NotImplementedException();
+            return a - b;
+        }
+
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        //generic calc method. usage: "10 + 20"  => result 30
+        public double Culculate(string expression)
+        {
+            throw new NotImplementedException();
+
+
+        }
+    }
+}
